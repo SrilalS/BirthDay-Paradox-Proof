4e274e354d5170f72fa13415ac08a3568fe04960
SrilalS <Srilalsachintha@Gmail.com>
2019-08-15 20:35:40 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,26 @@
+import random
+from collections import Counter
+import matplotlib.pyplot as plt
+
+PROB_ARR = []
+DAYS_ARR = []
+MAIN_CNT = 0
+
+
+def RDG():
+    DAYS_ARR.append(random.randrange(1, 365))
+
+
+def ARPRO(DAYS_ARR):
+    DAYS_ARR.sort()
+    DAYS_ARR = Counter(DAYS_ARR)
+    DAYS_ARR = DAYS_ARR.most_common(1)
+    return DAYS_ARR[0]
+
+def RUNNER():
+    for x in range(50):
+        RDG()
+    L = ARPRO(DAYS_ARR)
+    print(L[1])
+
+RUNNER()
